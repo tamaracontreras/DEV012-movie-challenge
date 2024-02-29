612075c933e3340e400a69c08a6ee4c984fad59e
Tam <tacs.laboral@gmail.com>
2024-02-29 14:03:53 -0300

    slint, jest,spec.js

---
+++ src/components/card.spec.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+// Importa la función del componente
+import { card } from './tu-componente';
+
+// Prueba para verificar si el componente crea correctamente los elementos esperados
+test('El componente card debe crear y devolver un elemento <section> con la información correcta', () => {
+	// Crea un objeto de prueba que simule un resultado de búsqueda
+	const primerResultado = {
+		Title: 'Título de la película',
+		Year: '2022',
+		Poster: 'url-de-la-imagen.jpg',
+	};
+
+	// Llama a la función del componente con el objeto de prueba
+	const section = card(primerResultado);
+
+	// Verifica si se ha creado un elemento <section>
+	expect(section.tagName).toBe('SECTION');
+
+	// Verifica si los elementos hijos esperados están presentes y tienen los valores correctos
+	const informacion = section.querySelector('.informacion');
+	expect(informacion).toBeTruthy();
+
+	const fondo = section.querySelector('.fondo');
+	expect(fondo).toBeTruthy();
+
+	const imgcont = section.querySelector('.imgcont');
+	expect(imgcont).toBeTruthy();
+
+	const imgElement = section.querySelector('img');
+	expect(imgElement).toBeTruthy();
+	expect(imgElement.src).toBe(primerResultado.Poster);
+
+	const contCard = section.querySelector('.contCard');
+	expect(contCard).toBeTruthy();
+
+	const idPelicula = section.querySelector('h4');
+	expect(idPelicula).toBeTruthy();
+	expect(idPelicula.textContent).toBe(primerResultado.Title);
+
+	const añoPelicula = section.querySelector('p');
+	expect(añoPelicula).toBeTruthy();
+	expect(añoPelicula.textContent).toBe(primerResultado.Year);
+});
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { searchMovies } from './api.js';
-import { card } from './components/card.js';
+
 
 // document.getElementById("root").appendChild(section);
 // en ete momento es cuando vamos a ejecutar esta peticion (evento boton buscar)
@@ -8,6 +8,6 @@ import { card } from './components/card.js';
 
 // Manejar el evento de clic en el botón de búsqueda
 document.getElementById('searchButton').addEventListener('click', () => {
-    // Llamar a la función searchMovies y manejar la promesa devuelta
-    searchMovies()
-})
+// Llamar a la función searchMovies y manejar la promesa devuelta
+	searchMovies();
+});
